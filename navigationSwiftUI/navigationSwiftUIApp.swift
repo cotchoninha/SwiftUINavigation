6e8eaa4d589f26f95f3b17999d6a4c002028fbed
Marcela Ceneviva Auslenter <cotchoninha@yahoo.com.br>
2022-01-04 20:38:00 +0000

    Initial Commit

---
+++ navigationSwiftUI/navigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationSwiftUIApp.swift
+//  navigationSwiftUI
+//
+//  Created by Marcela Auslenter on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct navigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
